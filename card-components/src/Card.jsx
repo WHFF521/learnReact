966e7a67f1c9f845e572b4a51d7be401cd7c9eae
WHFF521 <WHFF521@OUTLOOK.COM>
2024-03-03 00:23:24 +0800

    add card

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div className="card">
+      <h2 className="card-title">Title</h2>
+      <p className="card-text">Text</p>
+    </div>
+  );
+}
+export default Card;
