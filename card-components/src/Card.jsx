9ab8bfdb66516ee72a58700b4f6851741af8faee
WHFF521 <WHFF521@OUTLOOK.COM>
2024-03-03 19:44:28 +0800

    card
    two cards

--- card-components/src/Card.jsx
+++ card-components/src/Card.jsx
@@ -1,9 +1,10 @@
-
+import profilePic from "./assets/101.jpg"
 function Card() {
   return (
     <div className="card">
-      <h2 className="card-title">Title</h2>
-      <p className="card-text">Text</p>
+      <img className="card-image" src={profilePic} alt="profile picture"></img>
+      <h2 className="card-title">Oshino Shinobu</h2>
+      <p className="card-text">I watch Youtube videos and write codes</p>
     </div>
   );
 }
